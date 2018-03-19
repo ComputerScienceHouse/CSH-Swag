0b6e6663f8e0bc2c704edb1e155c0a66bc5feb2d
Devin Matte <devinmatte@gmail.com>
2018-03-19 15:59:10 -0400

    Caching ldap

--- Swag/ldap.py
+++ Swag/ldap.py
@@ -1,12 +1,15 @@
+from functools import lru_cache
+
 from Swag import _ldap
 
 
+@lru_cache(maxsize=1024)
 def ldap_is_financial(uid):
     financial = _ldap.get_directorship_heads("financial")
     return financial[0].get("uid")[0] == uid
 
 
+@lru_cache(maxsize=1024)
 def get_current_students():
-    active_members = [{"uid": member.get("uid")[0], "cn": member.get("cn")[0]} for member in
-                      _ldap.get_group('current_student').get_members()]
-    return active_members
+    return [{"uid": member.get("uid")[0], "cn": member.get("cn")[0]} for member in
+            _ldap.get_group('current_student').get_members()]
